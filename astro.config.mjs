1b543e5dd3372e215364ccc609c7c26b9d16eeb0
Myriad-Dreamin <camiyoru@gmail.com>
2025-05-24 17:58:32 +0800

    feat: update site loc

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,11 @@ import { URL_BASE } from "./config.json";
 // https://astro.build/config
 export default defineConfig({
   // Deploys to GitHub Pages
-  site: "https://myriad-dreamin.github.io",
+  // site: "https://myriad-dreamin.github.io",
+  // base: "/blog/",
+
+  // Deploys to My Blog Site
+  site: "https://www.myriad-dreamin.com",
   base: URL_BASE,
 
   integrations: [
