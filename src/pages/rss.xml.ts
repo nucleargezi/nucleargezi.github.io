c723db49b489b6cbd450a6f8f4aecb4625fd5e6a
Myriad-Dreamin <camiyoru@gmail.com>
2025-05-24 18:47:20 +0800

    feat: generate correct archive link

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,29 +4,32 @@ import type { CollectionEntry } from "astro:content";
 import type { APIContext } from "astro";
 import { kUrlBase, kSiteTitle, kSiteDescription } from "$consts";
 
-type Item = CollectionEntry<"blog" | "archive">;
+type Kind = "blog" | "archive";
+type Item = CollectionEntry<Kind>;
+const fromCollection = async (c: Kind, sub: string, suf: string) =>
+  (await getCollection(c)).map(
+    (item: Item): RSSFeedItem => ({
+      title: item.data.title,
+      description: item.data.description,
+      pubDate: item.data.date,
+      categories: item.data.tags,
+      link: `${kUrlBase}/${sub}/${item.id}${suf}`,
+    })
+  );
 
 export async function GET(context: APIContext) {
   if (!context.site) {
     throw new Error("No site URL found");
   }
 
-  const toRssFeed =
-    (sub: string) =>
-    (item: Item): RSSFeedItem => ({
-      title: item.data.title,
-      description: item.data.description,
-      pubDate: item.data.date,
-      categories: item.data.tags,
-      link: `${kUrlBase}/${sub}/${item.id}/`,
-    });
-
-  const posts = (await getCollection("blog")).map(toRssFeed("article"));
-  const archives = (await getCollection("archive")).map(toRssFeed("archive"));
+  const items = await Promise.all([
+    fromCollection("blog", "article", "/"),
+    fromCollection("archive", "archive", ".pdf"),
+  ]);
   return rss({
     title: kSiteTitle,
     description: kSiteDescription,
     site: context.site,
-    items: [...posts, ...archives],
+    items: items.flat(),
   });
 }
