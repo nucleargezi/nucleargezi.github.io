292f57f7cf6367a6145eb55b238affd039224ba3
Myriad-Dreamin <camiyoru@gmail.com>
2025-05-27 18:32:45 +0800

    fix: format error

--- scripts/pull-data.mjs
+++ scripts/pull-data.mjs
@@ -1,5 +1,10 @@
-import config from "../config.json" with { type: "json" };
 import { execSync } from "child_process";
+import { readFile } from "fs/promises";
+import { join } from "path";
+
+const config = JSON.parse(
+  await readFile(join(import.meta.dirname, "../config.json"), "utf-8")
+);
 
 const firstBackendUrl = config.BACKEND_ADDR[0];
 if (!firstBackendUrl) {
@@ -15,7 +20,10 @@ if (!firstBackendUrl) {
  * @param {string} dest - The destination file to save the pulled data.
  */
 const pullData = (route, dest) => {
-  const url = new URL(route, firstBackendUrl + (route.endsWith("/") ? "" : "/"));
+  const url = new URL(
+    route,
+    firstBackendUrl + (route.endsWith("/") ? "" : "/")
+  );
   try {
     console.log(`Pulling data from ${url}...`);
     execSync(`curl -s ${url} > ${dest}`, {
@@ -24,25 +32,22 @@ const pullData = (route, dest) => {
     console.log(`Data pulled successfully to ${dest}`);
     return true;
   } catch (error) {
-    console.error(
-      `Failed to pull data from ${url}:`,
-      error
-    );
+    console.error(`Failed to pull data from ${url}:`, error);
   }
 
   return false;
 };
 
-let okay = 
+let okay =
   pullData("snapshot/stats", "content/snapshot/article-stats.json") &&
   pullData("snapshot/comments", "content/snapshot/article-comments.json");
 
-  if (okay) {
-    // git diff
-    const result = execSync("git diff HEAD", {
-      stdio: "pipe",
-      cwd: "content/snapshot",
-      encoding: "utf-8",
-    });
-    console.log("Git diff result:\n", result);
-  }
+if (okay) {
+  // git diff
+  const result = execSync("git diff HEAD", {
+    stdio: "pipe",
+    cwd: "content/snapshot",
+    encoding: "utf-8",
+  });
+  console.log("Git diff result:\n", result);
+}
