e6d6c936e98fe6d81ae453200c7a76bd60323632
Myriad-Dreamin <camiyoru@gmail.com>
2025-05-28 12:05:17 +0800

    refactor: rearrange index.ts

--- packages/theme-toggle/index.ts
+++ packages/theme-toggle/index.ts
@@ -1,9 +1,8 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import ThemeToggle from "./src/ThemeToggle.astro";
-
 import ThemeInit from "./src/ThemeInit.astro";
+import ThemeToggle from "./src/ThemeToggle.astro";
 
 export { ThemeInit, ThemeToggle };
 export default ThemeToggle;
