95978d20443cf03c7e48990991c6f66eb9f75856
Myriad-Dreamin <camiyoru@gmail.com>
2025-05-28 11:14:08 +0800

    feat: split theme toggle

---
+++ packages/theme-toggle/index.ts
@@ -0,0 +1,9 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+import ThemeToggle from "./src/ThemeToggle.astro";
+
+import ThemeInit from "./src/ThemeInit.astro";
+
+export { ThemeInit, ThemeToggle };
+export default ThemeToggle;
