64e39de1e2c75815372faaaa792cdb5134eea2ea
nucleargezi <604223110@qq.com>
2026-03-21 06:01:51 +0800

    save

--- src/data/algorithm-tree-for-test.ts
+++ src/data/algorithm-tree-for-test.ts
@@ -17,12 +17,12 @@ export const algorithmTree = [
       {
         label: "Formal Power Series",
         children: [
-          { label: "FPS Note 0", slug: "a-20251216_fps0"},
-          { label: "FPS Note 1", slug: "a-20251217_fps1"},
-          { label: "FPS Note 2", slug: "a-20251218_fps2"},
-          { label: "FPS Training Rec", slug: "a-20251225_fps_train_rec"},
-          { label: "EGF Note", slug: "a-20260117_fps_egf"},
-          { label: "FPS 24 Rec", slug: "a-20260201_fps24"},
+          // { label: "FPS Note 0", slug: "a-20251216_fps0"},
+          // { label: "FPS Note 1", slug: "a-20251217_fps1"},
+          // { label: "FPS Note 2", slug: "a-20251218_fps2"},
+          // { label: "FPS Training Rec", slug: "a-20251225_fps_train_rec"},
+          // { label: "EGF Note", slug: "a-20260117_fps_egf"},
+          // { label: "FPS 24 Rec", slug: "a-20260201_fps24"},
         ]
       },
       {
@@ -36,13 +36,13 @@ export const algorithmTree = [
   {
     label: "Game",
     children: [
-      { label: "Base", slug: "a-20260202_game" },
+      // { label: "Base", slug: "a-20260202_game" },
     ],
   },
   {
     label: "Other",
     children: [
-      { label: "Major Voting", slug: "a-20251025_major_voting" },
+      // { label: "Major Voting", slug: "a-20251025_major_voting" },
     ],
   },
 ] satisfies AlgorithmTreeNode[];
