1621e80008ea1ccc6debcecc1b00e37b968faaf8
nucleargezi <604223110@qq.com>
2026-03-25 02:19:28 +0800

    sv

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { typst } from "astro-typst";
 
 export default defineConfig({
   site: "https://nucleargezi.github.io/next-blog",
+  base: "/next-blog",
   integrations: [
     typst({
       mode: {
